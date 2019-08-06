6408f45314d2b3ce5d52fe78b6b8705759c51f27
squishy123 <christian.gnaw@gmail.com>
2019-08-06 14:26:43 -0400

    Added make templates

--- scripts/templates/make.js
+++ scripts/templates/make.js
@@ -1,4 +1,5 @@
-export const MAKE_MIGRATION = `export default class %Migration% {
+export const MAKE_MIGRATION = `
+export default class %MIGRATION% {
     //Migration update
     up() {
 
@@ -8,4 +9,24 @@ export const MAKE_MIGRATION = `export default class %Migration% {
     down() {
 
     }
+}`;
+
+export const MAKE_MODEL = `
+import mongoose from 'mongoose';
+
+//Defined Schema
+const Schema = mongoose.Schema({});
+
+let %MODEL% = mongoose.model('%MODEL%', Schema);
+
+//Model Functions
+%MODEL%.getAll = () => %MODEL%.find({});
+
+export default %MODEL%`;
+
+export const MAKE_ROUTE = `
+export default {
+    method: "%METHOD%",
+    path: "%PATH%",
+    handler: []
 }`;
